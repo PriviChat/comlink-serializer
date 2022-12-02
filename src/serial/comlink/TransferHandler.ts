ce57e2bb1d55cabe60348bc92464486add2d67fc
Josh Manton <josh.manton@privichat.com>
2022-12-02 02:53:27 -0500

    refactor: renamed Handler to TransferHandler

---
+++ src/serial/comlink/TransferHandler.ts
@@ -0,0 +1,36 @@
+import * as Comlink from 'comlink';
+import { Deserializer } from '../Deserializer';
+import { Serializable } from '..';
+import { Serialized } from '../types';
+import { AnyConstructor } from '../mixin/types';
+import { SerializableArray, SerializableMap } from '../../serialobjs';
+
+/* Global Handlers */
+
+export interface TransferHandlerRegistration {
+	transferClasses: AnyConstructor<Serializable>[];
+}
+
+export function registerTransferHandler(config: TransferHandlerRegistration) {
+	//Declare these so that the decorators get called (and the classes registered in the registry)
+	SerializableArray;
+	SerializableMap;
+
+	Comlink.transferHandlers.set('SerializableObject', serializableObjectTransferHandler);
+}
+
+export const serializableObjectTransferHandler: Comlink.TransferHandler<Serializable, Serialized> = {
+	// We want to use this transfer handler for any objects that have the serializable mixin
+	canHandle: function (value: any): value is Serializable {
+		return value?.isSerializable ?? false;
+	},
+	serialize: (object) => {
+		// Convert the UserId to string for transfer
+		const serialized = object.serialize();
+		return [serialized, []];
+	},
+	deserialize: (object) => {
+		const deserialized = Deserializer.deserialize(object);
+		return deserialized;
+	},
+};
